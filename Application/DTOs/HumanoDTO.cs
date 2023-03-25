3d7ad1cb3233154cc523cca4bed1b960777e53c2
Luis Armando <armando_ze94@hotmail.com>
2023-03-25 13:55:16 -0600

    Se crea modelo y dto

--- Application/DTOs/HumanoDTO.cs
+++ Application/DTOs/HumanoDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace Application.DTOs
 {
     public class HumanoDTO
     {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Sexo { get; set; }
+        [Required]
+        public int Edad { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
     }
 }
