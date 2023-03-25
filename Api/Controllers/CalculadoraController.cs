e2f7f3f32c1e66aa62f21a72c2502c8ede46742f
Luis Armando <armando_ze94@hotmail.com>
2023-03-25 16:36:40 -0600

    Creacion de controller calculadora

---
+++ Api/Controllers/CalculadoraController.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet("{valor1}{valor2}{valor3}")]
+        public int GetSuma(int valor1, int valor2, int valor3)
+        {
+            return Sumar(valor1, valor2, valor3);
+        }
+        [HttpPost("{valor1}{valor2}{valor3}")]
+        public int PostSuma(int valor1, int valor2, int valor3)
+        {
+            return Sumar(valor1, valor2, valor3);
+        }
+        private static int Sumar(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
